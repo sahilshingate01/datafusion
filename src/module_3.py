02dd9636b244604ad00225a489cc797f281aadb9
Sahil Shingate <sahilshingate0@gmail.com>
2025-09-27 12:03:00 +0530

    Add comments for clarity

--- src/module_3.py
+++ src/module_3.py
@@ -4,3 +4,5 @@ print('Commit 3 on 2025-09-04')
 print('Commit 3 on 2025-09-09')
 # Auto-generated module 3
 print('Commit 3 on 2025-09-25')
+# Auto-generated module 3
+print('Commit 3 on 2025-09-27')
